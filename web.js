276ac028bc5d588e86fbfbcf1b0fe0cb46e77c34
Alex Salgado <alexsalgado1@gmail.com>
2013-07-09 19:18:56 +0000

    Updated web.js read from index.html

--- web.js
+++ web.js
@@ -3,7 +3,11 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  //read from index.html
+  
+  var textToShow = fs.readFileSync ('index.html', 'utf-8');
+  response.send (textToShow );
+
 });
 
 var port = process.env.PORT || 5000;
