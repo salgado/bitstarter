e65f394696868b92451efdee3e3c68539192e3b7
Alex Salgado <alexsalgado1@gmail.com>
2013-07-09 22:54:28 +0000

    Updated2 web.js read from index.html

--- web.js
+++ web.js
@@ -4,11 +4,14 @@ var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
+
 app.get('/', function(request, response) {
   //read from index.html
   
-  var textToShow = fs.readFileSync ("index.html", "utf-8");
-  response.send ( textToShow );
+  var textToShow = fs.readFileSync ("index.html", "utf8");
+  var buf = new Buffer (textToShow);
+
+  response.send ( buf.toString('utf8') );
 
 });
 
