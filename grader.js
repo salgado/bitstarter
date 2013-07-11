a2b69239405f7e6a8f77c33e7e69f31cfbea4b3f
Alex Salgado <alexsalgado1@gmail.com>
2013-07-11 00:59:37 +0000

    Updated grader.js for url

--- grader.js
+++ grader.js
@@ -26,6 +26,7 @@ var program = require('commander');
 var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var rest = require('restler');
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -40,12 +41,36 @@ var cheerioHtmlFile = function(htmlfile) {
     return cheerio.load(fs.readFileSync(htmlfile));
 };
 
+var cheerioHtmlUrl = function(response) {
+	var retHtml;
+
+    if (response instanceof Error) {
+     	console.log("%s does not exist. Exiting.", response.message);
+     	process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
+    } else {
+        retHtml = cheerio.load(response);
+        return retHtml;
+    }
+
+};
+
 var loadChecks = function(checksfile) {
     return JSON.parse(fs.readFileSync(checksfile));
 };
 
 var checkHtmlFile = function(htmlfile, checksfile) {
     $ = cheerioHtmlFile(htmlfile);
+    var checks = loadChecks(checksfile).sort();
+    var out = {};
+    for(var ii in checks) {
+        var present = $(checks[ii]).length > 0;
+        out[checks[ii]] = present;
+    }
+    return out;
+};
+
+var checkHtmlUrl = function(response, checksfile) {
+    $ = cheerioHtmlUrl(response);
     var checks = loadChecks(checksfile).sort();
     var out = {};
     for(var ii in checks) {
@@ -65,10 +90,26 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <url_html_file>', 'Path to url' )
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+    
+    var checkJson;
+    var outJson;
+    
+    //test the origin of file to compare
+    if ( program.url) {
+		rest.get(program.url).on('complete', function(response) {
+	    	checkJson = checkHtmlUrl ( response, program.checks );
+		    outJson = JSON.stringify(checkJson, null, 4);
+		    console.log(outJson);
+	    });
+    }
+    else {   
+        checkJson = checkHtmlFile(program.file, program.checks);    
+	    outJson = JSON.stringify(checkJson, null, 4);
+	    console.log(outJson);
+	}
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
+
